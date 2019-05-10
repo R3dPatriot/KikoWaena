6c7bf17594de03fa009b01ca9c40709e4ca1c18c
R3dPatriot <39312151+R3dPatriot@users.noreply.github.com>
2019-05-10 17:33:53 +0200

    Base unreport.php skeleton
    Added switch statement on the request type.
    Data sending and treatment to add.
    Page generation to add.

--- php/ajax/unreport.php
+++ php/ajax/unreport.php
@@ -2,3 +2,19 @@
 
 require_once('../database/admin_stubs.php');
 
+//Checks the request
+$requestType = $_SERVER['REQUEST_METHOD'];
+/*$request = substr($_SERVER['PATH_INFO'], 1);
+$request = explode('/', $request);
+$requestRessource = array_shift($request);*/
+
+switch ($requestType) {
+    case 'PUT':
+        break;
+    case 'DELETE':
+        break;
+    default:
+        header('HTTP/1.1 400 Bad Request');
+        exit;
+}
+exit;
