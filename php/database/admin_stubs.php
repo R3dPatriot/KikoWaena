3f1519824f5bfac08dd851ebac721422451b9ef1
R3dPatriot <39312151+R3dPatriot@users.noreply.github.com>
2019-05-10 17:14:38 +0200

    Added false signaled photos
    Added users 1 and 2 with photos for test purposes.
    Added those photos in an array in the stubRecupElementsSignales method.

--- php/database/admin_stubs.php
+++ php/database/admin_stubs.php
@@ -7,7 +7,7 @@ function stubDeleteElement($type, $id) {
     if ($type !== 'photo' || $type !== 'comment') {
         return false;
     } else {
-        return true;
+        return $id;
     }
 }
 
@@ -15,13 +15,19 @@ function stubUnflagElement($type, $id) {
     if ($type !== 'photo' || $type !== 'comment') {
         return false;
     } else {
-        return true;
+        return $id;
     }
 }
 
-function recupElementSignales($type) {
-    $reportedPhotos = array();
+function stubRecupElementsSignales($type) {
+    $photo1 = new Photo(1, 1, 'upgradePHP', 'Progmemer', 'Elouen');
+    $photo2 = new Photo(2, 1, 'placesNantes', 'Progmemer', 'Elouen');
+    $photo3 = new Photo(1, 2, 'Avengers', 'ISEN-MEMER', 'Alex-DEP');
+    $reportedPhotos = array($photo1, $photo2, $photo3);
 
     if($type === 'photo') {
+        return $reportedPhotos;
+    } else {
+        return false;
     }
 }
